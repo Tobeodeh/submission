057eb8b816b2ff6f94ac5fb0912abd7ef6ba174d
Tobechukwu Chris-Odeh <108990991+Tobeodeh@users.noreply.github.com>
2022-12-16 21:53:02 +0100

    Add files via upload

---
+++ Program22.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ppo
+{
+    class Program
+    {
+        public void up(string sum)
+           
+        {
+            int length = sum.Length;
+            int count = 0;
+            
+            for(int i = 0; i<length; i++)
+            {
+                if (sum[i] == 'u')
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+
+                Console.WriteLine(count) ;
+            }
+        }
+    
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ent the numbe of steps to check");
+            int no_times = int.Parse(Console.ReadLine());
+            for(int j = 0; j < no_times; j++)
+            {
+               string step = Console.ReadLine();
+
+                Program Step = new Program();
+                Step.up(step);
+
+            }
+        }
+    }
+}
